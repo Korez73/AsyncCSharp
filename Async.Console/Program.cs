1e52bc7b36b27ade9f8d68e7a3d53a7720ff8066
Jamie Weldon <jamie.weldon@outlook.com>
2024-08-28 14:52:58 -0500

    initial commit

---
+++ Async.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿void ProcessData()
+{
+  Console.WriteLine("Processing Started");
+  Thread.Sleep(10000);
+  Console.WriteLine("Processing Completed");
+}
+
+Console.WriteLine("Program executing...");
+
+//this is the most simple way to run a task
+//Task.Run(ProcessData);
+
+//this is a good idea if you need to conditionally start a task
+var dataTask = new Task(ProcessData);
+dataTask.Start();
+
+Console.WriteLine("Ready for user input.");
+Console.ReadLine();
