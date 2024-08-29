ac27968a5daf8a54a0df0d8353c6767805697d2e
Jamie Weldon <jamie.weldon@outlook.com>
2024-08-29 10:32:34 -0500

    continuations and async/await

---
+++ Async.Console.HttpClient/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Program executing...");
+
+
+
+
+//this is blocking
+/*
+var responseTask = new HttpClient().GetAsync("http://www.fark.com");
+var response = responseTask.Result;
+Console.WriteLine(response.StatusCode);
+*/
+
+//using a continuation not to block
+/*
+var responseTask = new HttpClient().GetAsync("http://www.fark.com");
+responseTask.ContinueWith(httpTask => {
+  var response = httpTask.Result;
+  Console.WriteLine(response.StatusCode);
+});
+*/
+
+//using async/await not to block
+async Task PrintStatusCodeAsync()
+{
+  var response = await new HttpClient().GetAsync("http://www.fark.com");
+  Console.WriteLine(response.StatusCode);
+}
+var statusCodeTask = PrintStatusCodeAsync();
+
+
+Console.WriteLine("Ready for user input!");
+Console.ReadLine();
