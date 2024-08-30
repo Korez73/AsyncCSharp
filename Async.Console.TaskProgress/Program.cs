01ce29e034b4e81e24f531036f8a99468081ab33
Jamie Weldon <jamie.weldon@outlook.com>
2024-08-30 06:35:46 -0500

    Task Progress Reporting

---
+++ Async.Console.TaskProgress/Program.cs
@@ -0,0 +1,18 @@
+﻿async Task ProcessData(string[] departments, IProgress<int> progress)
+{
+  for(int i = 1; i <= departments.Count(); i++)
+  {
+    progress.Report(i);
+    //simulate data processing
+    await Task.Delay(2000);
+  }
+  Console.WriteLine("Done processing data.");
+}
+
+Console.WriteLine("Program is running.");
+
+string[] departments = { "Engineering", "Sales", "Marketing", "Support"};
+IProgress<int> progressReporter = new Progress<int>(i => Console.WriteLine($"Processing department {i}"));
+var dataTask = ProcessData(departments, progressReporter);
+Console.WriteLine("Ready for user input.");
+await dataTask;
